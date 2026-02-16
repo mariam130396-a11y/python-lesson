cff8a3e1205e2f9f09afbae0e5fe111e7ceb7ad0
mariam130396-a11y <mariam130396@gmail.com>
2026-02-16 16:26:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+from transliterate import translit
+from num2words import num2words
+import re
+
+
 print( """Ladies and gentlemen, I'm 78 years old and I finally got 15 minutes of fame once in a lifetime
 and I guess that this is mine.
 
@@ -7,25 +12,17 @@ and to take vengeance of my enemies. Neither will happen.
 More than 3 years ago I moved to Novo-Novsk, but worked on new Magnetic Storage for last 40.
 When I was 8...
 """ ) 
-from transliterate import translit
 print (translit( "Lorem ipsum dolor sit amet", 'hy'))
 print (translit ("Lorem ipsum dolor sit amet" , 'el'))
 print (translit ( "Lorem ipsum dolor sit amet", 'uk'))
-
-from transliterate import translit
 print (translit ("""Ladies and gentlemen, I'm 78 years old and I finally got 15 minutes of fame once in a lifetime
 and I guess that this is mine. People have also told me to make these next few minutes
 excruciatingly embarrassing and to take vengeance of my enemies. Neither will happen.
 More than 3 years ago I moved to Novo-Novsk, but worked on new Magnetic Storage for last 40.
 When I was 8...""", 'ru'))
-
-from num2words import num2words
-
 print(num2words(42))
 print(num2words(42, to='ordinal'))
 print(num2words(42, lang='fr'))
-
-from transliterate import translit
 
 print(translit( """Ladies and gentlemen, I'm 78 years old and I finally got 15 minutes of fame once in a lifetime
 and I guess that this is mine.
@@ -36,9 +33,6 @@ and to take vengeance of my enemies. Neither will happen.
 More than 3 years ago I moved to Novo-Novsk, but worked on new Magnetic Storage for last 40.
 When I was 8...
 """, 'ru' )) 
-import re
-from num2words import num2words
-
 speech = """
 Ладиес анд гентлемен, ИЬм 78 ыеарс олд анд И финаллы гот 15 минутес оф фаме онце ин а лифетиме
 анд И гуесс тхат тхис ис мине. Пеопле хаве алсо толд ме то маке тхесе неxт феw минутес
@@ -54,9 +48,6 @@ numbers = re.findall(r'\d+', speech)
 for number in numbers:
     print(number, "-", num2words(int(number)))
 
-import re
-from num2words import num2words
-from transliterate import translit
 
 speech = """
 Ладиес анд гентлемен, ИЬм 78 ыеарс олд анд И финаллы гот 15 минутес
