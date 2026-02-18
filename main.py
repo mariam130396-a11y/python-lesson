ac06127aad8a4f8805f4511031f02214625c8b12
mariam130396-a11y <mariam130396@gmail.com>
2026-02-18 22:10:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,26 +3,11 @@ from num2words import num2words
 import re
 
 
-print( """Ladies and gentlemen, I'm 78 years old and I finally got 15 minutes of fame once in a lifetime
-and I guess that this is mine.
-
-People have also told me to make these next few minutes excruciatingly embarrassing
-and to take vengeance of my enemies. Neither will happen.
-
-More than 3 years ago I moved to Novo-Novsk, but worked on new Magnetic Storage for last 40.
-When I was 8...
-""" ) 
-print (translit( "Lorem ipsum dolor sit amet", 'hy'))
-print (translit ("Lorem ipsum dolor sit amet" , 'el'))
-print (translit ( "Lorem ipsum dolor sit amet", 'uk'))
 print (translit ("""Ladies and gentlemen, I'm 78 years old and I finally got 15 minutes of fame once in a lifetime
 and I guess that this is mine. People have also told me to make these next few minutes
 excruciatingly embarrassing and to take vengeance of my enemies. Neither will happen.
 More than 3 years ago I moved to Novo-Novsk, but worked on new Magnetic Storage for last 40.
 When I was 8...""", 'ru'))
-print(num2words(42))
-print(num2words(42, to='ordinal'))
-print(num2words(42, lang='fr'))
 
 print(translit( """Ladies and gentlemen, I'm 78 years old and I finally got 15 minutes of fame once in a lifetime
 and I guess that this is mine.
@@ -44,16 +29,6 @@ Wхен И wас 8...
 # найти все числа
 numbers = re.findall(r'\d+', speech)
 
-# вывести число и слово
-for number in numbers:
-    print(number, "-", num2words(int(number)))
-
-
-speech = """
-Ладиес анд гентлемен, ИЬм 78 ыеарс олд анд И финаллы гот 15 минутес
-Море тхан 3 ыеарс аго... ласт 40. Wхен И wас 8...
-"""
-
 # найти все числа
 numbers = re.findall(r'\d+', speech)
 
@@ -61,6 +36,6 @@ numbers = re.findall(r'\d+', speech)
 for number in numbers:
     word_en = num2words(int(number))
     word_ru = translit(word_en, 'ru')
-    print(number, "-", word_en, "-", word_ru)
+    
 
 
