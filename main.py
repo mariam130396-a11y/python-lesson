e490ba84eeaca8ef11258ab1aa622441ce996753
mariam130396-a11y <mariam130396@gmail.com>
2026-02-19 20:53:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,16 +8,6 @@ and I guess that this is mine. People have also told me to make these next few m
 excruciatingly embarrassing and to take vengeance of my enemies. Neither will happen.
 More than 3 years ago I moved to Novo-Novsk, but worked on new Magnetic Storage for last 40.
 When I was 8...""", 'ru'))
-
-print(translit( """Ladies and gentlemen, I'm 78 years old and I finally got 15 minutes of fame once in a lifetime
-and I guess that this is mine.
-
-People have also told me to make these next few minutes excruciatingly embarrassing
-and to take vengeance of my enemies. Neither will happen.
-
-More than 3 years ago I moved to Novo-Novsk, but worked on new Magnetic Storage for last 40.
-When I was 8...
-""", 'ru' )) 
 speech = """
 Ладиес анд гентлемен, ИЬм 78 ыеарс олд анд И финаллы гот 15 минутес оф фаме онце ин а лифетиме
 анд И гуесс тхат тхис ис мине. Пеопле хаве алсо толд ме то маке тхесе неxт феw минутес
